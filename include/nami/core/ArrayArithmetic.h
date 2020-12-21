4e23b559bcc0aed6e151b534c0e721718b2fbfff
David Huber <dave@yomi.eu>
2020-12-21 21:54:27 +0100

    Extend array arithmetic

--- include/nami/core/ArrayArithmetic.h
+++ include/nami/core/ArrayArithmetic.h
@@ -8,34 +8,63 @@
 #include <algorithm>
 #include <array>
 #include <cassert>
+#include <concepts>
 
 namespace nami::core::array {
 
-  template <typename Iterable>
-  inline constexpr Iterable& operator+=(Iterable& lhs, const Iterable& rhs) {
+  namespace internal {
+    template <typename T> concept Iterable = requires(T a) {
+      {std::begin(a)};
+      {std::end(a)};
+      {std::size(a)};
+    };
+  }  // namespace internal
+
+  inline constexpr auto& operator+=(internal::Iterable auto& lhs, internal::Iterable auto& rhs) {
     assert(std::size(lhs) == std::size(rhs));
     std::transform(std::begin(lhs), std::end(lhs), std::begin(rhs), std::begin(lhs),
                    [](const auto& a, const auto& b) { return a + b; });
     return lhs;
   }
 
-  template <typename Iterable>
-  inline constexpr Iterable operator+(Iterable lhs, const Iterable& rhs) {
+  inline constexpr auto operator+(internal::Iterable auto lhs, const internal::Iterable auto& rhs) {
     lhs += rhs;
     return lhs;
   }
 
-  template <typename Iterable>
-  inline constexpr Iterable& operator-=(Iterable& lhs, const Iterable& rhs) {
+  inline constexpr auto& operator-=(internal::Iterable auto& lhs, internal::Iterable auto& rhs) {
     assert(std::size(lhs) == std::size(rhs));
     std::transform(std::begin(lhs), std::end(lhs), std::begin(rhs), std::begin(lhs),
                    [](const auto& a, const auto& b) { return a - b; });
     return lhs;
   }
 
-  template <typename Iterable>
-  inline constexpr Iterable operator-(Iterable lhs, const Iterable& rhs) {
+  inline constexpr auto operator-(internal::Iterable auto lhs, const internal::Iterable auto& rhs) {
     lhs -= rhs;
+    return lhs;
+  }
+
+  inline constexpr auto& operator/=(internal::Iterable auto& lhs, internal::Iterable auto& rhs) {
+    assert(std::size(lhs) == std::size(rhs));
+    std::transform(std::begin(lhs), std::end(lhs), std::begin(rhs), std::begin(lhs),
+                   [](const auto& a, const auto& b) { return a / b; });
+    return lhs;
+  }
+
+  inline constexpr auto operator/(internal::Iterable auto lhs, const internal::Iterable auto& rhs) {
+    lhs /= rhs;
+    return lhs;
+  }
+
+  inline constexpr auto& operator*=(internal::Iterable auto& lhs, internal::Iterable auto& rhs) {
+    assert(std::size(lhs) == std::size(rhs));
+    std::transform(std::begin(lhs), std::end(lhs), std::begin(rhs), std::begin(lhs),
+                   [](const auto& a, const auto& b) { return a * b; });
+    return lhs;
+  }
+
+  inline constexpr auto operator*(internal::Iterable auto lhs, const internal::Iterable auto& rhs) {
+    lhs *= rhs;
     return lhs;
   }
 
--- test/source/ArrayArithmetic.cpp
+++ test/source/ArrayArithmetic.cpp
@@ -10,22 +10,86 @@
 using namespace nami;
 using namespace nami::core::array;
 
-TEST_CASE("Array Addition") {
+TEST_CASE("Array Array Arithmetic") {
   std::array<double, 2> a{1, 2};
   std::array<double, 2> b{10, 20};
 
-  auto c = a + b;
-
-  CHECK(c[0] == 11);
-  CHECK(c[1] == 22);
+  SUBCASE("Addition") {
+    auto c = a + b;
+    CHECK(c[0] == 11);
+    CHECK(c[1] == 22);
+  }
+  SUBCASE("Subtraction") {
+    auto c = a - b;
+    CHECK(c[0] == -9);
+    CHECK(c[1] == -18);
+  }
+  SUBCASE("Multiplication") {
+    auto c = a * b;
+    CHECK(c[0] == 10);
+    CHECK(c[1] == 40);
+  }
+  SUBCASE("Division") {
+    auto c = b / a;
+    CHECK(c[0] == 10);
+    CHECK(c[1] == 10);
+  }
+  SUBCASE("Implicit Typecast") {
+    std::array<std::size_t, 2> d{2, 8};
+    auto c = a / d;
+    CHECK(c[0] == 0.5);
+    CHECK(c[1] == 0.25);
+  }
 }
 
-TEST_CASE("Array Subtraction") {
+TEST_CASE("Array Vector Arithmetic") {
   std::array<double, 2> a{1, 2};
+  std::vector<double> b{10, 20};
+
+  SUBCASE("Addition") {
+    auto c = a + b;
+    CHECK(c[0] == 11);
+    CHECK(c[1] == 22);
+  }
+  SUBCASE("Subtraction") {
+    auto c = a - b;
+    CHECK(c[0] == -9);
+    CHECK(c[1] == -18);
+  }
+  SUBCASE("Multiplication") {
+    auto c = a * b;
+    CHECK(c[0] == 10);
+    CHECK(c[1] == 40);
+  }
+  SUBCASE("Division") {
+    auto c = b / a;
+    CHECK(c[0] == 10);
+    CHECK(c[1] == 10);
+  }
+}
+
+TEST_CASE("Vector Array Arithmetic") {
+  std::vector<double> a{1, 2};
   std::array<double, 2> b{10, 20};
 
-  auto c = a - b;
-
-  CHECK(c[0] == -9);
-  CHECK(c[1] == -18);
+  SUBCASE("Addition") {
+    auto c = a + b;
+    CHECK(c[0] == 11);
+    CHECK(c[1] == 22);
+  }
+  SUBCASE("Subtraction") {
+    auto c = a - b;
+    CHECK(c[0] == -9);
+    CHECK(c[1] == -18);
+  }
+  SUBCASE("Multiplication") {
+    auto c = a * b;
+    CHECK(c[0] == 10);
+    CHECK(c[1] == 40);
+  }
+  SUBCASE("Division") {
+    auto c = b / a;
+    CHECK(c[0] == 10);
+    CHECK(c[1] == 10);
+  }
 }
